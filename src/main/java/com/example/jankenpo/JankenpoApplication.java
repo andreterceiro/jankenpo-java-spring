9299274852ecb216a4c8a5d1971152dd930d59d7
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-10-12 12:17:01 -0300

    feat: evolving

--- src/main/java/com/example/jankenpo/JankenpoApplication.java
+++ src/main/java/com/example/jankenpo/JankenpoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JankenpoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JankenpoApplication.class, args);
 	}
-
 }
