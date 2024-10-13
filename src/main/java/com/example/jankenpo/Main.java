4f06cd348651c826021f7bb6393c1a985e7c078f
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-10-13 11:41:30 -0300

    feat: updating the command line runner instance

--- src/main/java/com/example/jankenpo/Main.java
+++ src/main/java/com/example/jankenpo/Main.java
@@ -1,5 +1,7 @@
 package com.example.jankenpo;
 
+import java.util.Scanner;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -7,6 +9,27 @@ import org.springframework.stereotype.Component;
 public class Main implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("oi");
+        String userOption = Main.receiveUserOption();
+
+        Jankenpo.evaluateAndShowResult(userOption);
     }
+
+    /**
+     * Receives the user option
+     * 
+     * @return String
+     */
+    private static String receiveUserOption() {
+    	Scanner scanner = new Scanner(System.in);
+    	String option = "";
+    	
+    	while ((!option.equals("R")) && (!option.equals("P")) && (!option.equals("S")) ) {
+        	System.out.print("Please insert 'P' for 'paper' or 'R' for 'rock' or 'S' for 'scissors': ");
+        	option = scanner.nextLine();
+    	}
+    	scanner.close();
+    	
+    	return option;
+    }
+
 }
